53f64d603aa4fe2a69c2a61a0d6de138b7aae29c
Charles-André Martineau <charlesandre_martineau@hotmail.fr>
2023-02-17 21:35:27 -0500

    Adding coordinates to the api calls

--- src/api/restaurants.ts
+++ src/api/restaurants.ts
@@ -8,18 +8,26 @@ export interface Restaurant {
   
 }
 
-
 interface APIResponse {
   places: Restaurant[];
   otherData: any;
 }
 
-export const getRestoClose = async (radius: number): Promise<Restaurant[]> => {
-  const response = await axios.get<APIResponse>(`https://api.radar.io/v1/search/places?categories=restaurant&near=45.540748%2C-73.630126&radius=${radius}`,{
+export const getRestoClose = async (radius: number,location_lat: number,location_long:number ): Promise<Restaurant[]> => {
+  const response = await axios.get<APIResponse>(`https://api.radar.io/v1/search/places?categories=restaurant&near=${location_lat}%2C${location_long}&radius=${radius}`,{
     headers: {
-      'Authorization' : ``
+      'Authorization' : 'prj_live_pk_dcddf31cdba335e899bd19b16367d601d58452b9'
   }
 });
+
   return response.data.places;
 };
 
+export const getRestodistance = async (id: number): Promise<number> => {
+  const response = await axios.get<APIResponse>(`https://api.radar.io/v1/search/places?categories=restaurant&near=45.540748%2C-73.630126&radius=${radius}`,{
+    headers: {
+      'Authorization' : 'prj_live_pk_dcddf31cdba335e899bd19b16367d601d58452b9'
+  }
+});
+  return 2;
+};
