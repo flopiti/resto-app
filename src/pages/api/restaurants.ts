0c7273501c0e63c66a26b5f5a33b3ccc1131ee70
flopiti <npieraut@live.fr>
2023-03-04 14:35:59 -0500

    fix compilation problems

--- src/pages/api/restaurants.ts
+++ src/pages/api/restaurants.ts
@@ -22,23 +22,23 @@ export const getRestoClose = async (radius: number,location_lat: number,location
   });
   return response.data.places;
   }
+  return [];
 };
 
-export const getRestodistance = async (id: number): Promise<number> => {
-  const response = await axios.get<APIResponse>(`https://api.radar.io/v1/search/places?categories=restaurant&near=45.540748%2C-73.630126&radius=${radius}`,{
-    headers: {
-      'Authorization': `${process.env.RADAR_API_KEY}`
-    }
-});
-  return 2;
-};
+// export const getRestodistance = async (id: number): Promise<number> => {
+//   const response = await axios.get<APIResponse>(`https://api.radar.io/v1/search/places?categories=restaurant&near=45.540748%2C-73.630126&radius=${radius}`,{
+//     headers: {
+//       'Authorization': `${process.env.RADAR_API_KEY}`
+//     }
+// });
+//   return 2;
+// };
 
 
 export default async function restaurants(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     try {
       const restaurants = await getRestoClose(Number(req.query.radius),Number(req.query.location_lat),Number(req.query.location_long));
-      console.log(restaurants)
       res.status(200).json(restaurants);
     } catch (error) {
       console.error(error);
