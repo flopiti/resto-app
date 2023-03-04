38504bfba71778f675d08d444ae6a88730f30383
flopiti <npieraut@live.fr>
2023-03-04 14:40:39 -0500

    walking box structure

---
+++ src/components/WalkingBox.tsx
@@ -0,0 +1,14 @@
+import styles from '../../styles/styles.module.css';
+
+export const WalkingBox = () => {
+    return (
+        <span className={styles.walkingBox}>
+            <div>
+                <h3>Walking</h3>
+            </div>
+            <div>
+                <p>Walking is a great way to get around.</p>
+            </div>
+        </span>
+    );
+    };
